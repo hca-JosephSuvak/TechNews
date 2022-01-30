6e8e6d23114241ab2b645e4b6539b3df6ecd0da0
Joseph Suvak <robert.suvak@hcahealthcare.com>
2022-01-29 23:31:07 -0600

    Lesson dos
    IntelliJ Generated Code

---
+++ tech-news-java-api/src/main/java/com/technews/model/Vote.java
@@ -0,0 +1,103 @@
+package com.technews.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "vote")
+public class Vote {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.AUTO)
+    private Integer Id;
+    private Integer userId;
+    private Integer postId;
+
+    public Vote() {
+
+    }
+
+    public Vote(Integer id, Integer userId, Integer postId){
+        this.Id = id;
+        this.userId = userId;
+        this.postId = postId;
+    }
+
+    public Vote(Integer userId, Integer postId){
+        this.userId = userId;
+        this.postId = postId;
+    }
+
+
+    /*
+    *
+    *
+    * Getters/Setters
+    *
+    * */
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Integer postId) {
+        this.postId = postId;
+    }
+
+    /*
+     *
+     *
+     * Equals & HashCode
+     *
+     * */
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Vote)) return false;
+        Vote vote = (Vote) o;
+        return Objects.equals(Id, vote.Id) &&
+                Objects.equals(userId, vote.userId) &&
+                Objects.equals(postId, vote.postId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Id, userId, postId);
+    }
+
+    /*
+     *
+     *
+     * toString
+     *
+     * */
+
+    @Override
+    public String toString() {
+        return "Vote{" +
+                "Id=" + Id +
+                ", userId=" + userId +
+                ", postId=" + postId +
+                '}';
+    }
+}
